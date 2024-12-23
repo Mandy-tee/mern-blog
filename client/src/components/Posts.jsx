cfc62c0c1fda64a0b60f46fc1d90cc2c1521f0c8
Amanda Tagoe <nayork77@gmail.com>
2024-12-23 00:13:02 +0000

    Post authors, items and details worked on

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Thumbnail1 from '../assets/images/blog1.jpg'
+import Thumbnail2 from '../assets/images/blog2.jpg'
+import Thumbnail3 from '../assets/images/blog3.jpg'
+import Thumbnail4 from '../assets/images/blog4.jpg'
+import PostItem from './PostItem'
+
+const DUMMY_POSTS = [
+    {
+        id: '1',
+        thumbnail: Thumbnail1,
+        category: 'education',
+        title: 'This is the title of the very first post on this blog.',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores obcaecati aspernatur dolores quidem, ab nemo consequuntur unde sequi odit itaque aperiam. Eligendi hic dignissimos, reiciendis nulla a dolor accusantium numquam.',
+        authorID: 3
+    },
+    {
+        id: '2',
+        thumbnail: Thumbnail2,
+        category: 'science',
+        title: 'Welcome to Ghana.',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, aliquam dolores quas esse, perspiciatis odit facilis voluptatibus nihil cupiditate a dicta, repellat repudiandae illo enim alias harum eveniet accusamus accusantium.',
+        authorID: 1
+    },
+    {
+        id: '3',
+        thumbnail: Thumbnail3,
+        category: 'weather',
+        title: 'This is the title of the very first post on this blog.',
+        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima quos ea quas optio voluptatibus sapiente laudantium harum vitae repellendus! Magni et blanditiis id similique eos soluta tempore deserunt consequatur voluptate!',
+        authorID: 13
+    },
+    {
+        id: '4',
+        thumbnail: Thumbnail4,
+        category: 'farming',
+        title: 'This is the title of the very first post on this blog.',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, cumque quibusdam. Neque dolor sequi nihil, modi ut dicta laborum, eos dolores ullam quibusdam asperiores itaque accusantium quam nam aut voluptates.',
+        authorID: 11
+    },
+]
 
 const Posts = () => {
-  return (
-    <div>Posts</div>
-  )
+    const [posts, setPosts] = useState(DUMMY_POSTS)
+    return (
+        <section className='posts'>
+            <div className='container posts_container'>
+                {
+                    posts.map(({ id, thumbnail, category, title, desc, authorID }) =>
+                        <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} />)
+                }
+            </div>
+        </section>
+    )
 }
 
 export default Posts
--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import PostAuthor from '../components/PostAuthor'
+import { Link } from 'react-router-dom'
+import Thumbnail from '../assets/images/blog22.jpg'
 
 const PostDetail = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='post-detail'>
+      <div className='container post-detail_container'>
+        <div className='post-detail_header'>
+          <PostAuthor />
+          <div className='post-detail_buttons'>
+            <Link to={`/posts/werwer/edit`} className='btn sm primary'>Edit</Link>
+            <Link to={`/posts/werwer/delete`} className='btn sm danger'>Delete</Link>
+          </div>
+        </div>
+        <h1>This is the post title</h1>
+        <div className='post-detail_thumbnail'>
+          <img src={Thumbnail} alt="" />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas facilis esse ipsum dolore quidem amet corporis ipsa praesentium iure. Quo odio praesentium, assumenda ab nobis voluptatum iusto qui excepturi vitae quasi sit amet numquam quaerat, molestias animi eligendi labore officia!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in inventore neque excepturi consequatur voluptas incidunt ipsam minima qui enim, quibusdam nesciunt eum consectetur voluptatum possimus, quidem, necessitatibus ratione fuga iusto harum. Eius aut assumenda suscipit dicta laudantium eligendi, dolorem iusto nobis iure quia repellendus odio laborum mollitia tempora cum modi sequi quas expedita. Cumque.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, dolor! Hic quasi minima mollitia, molestiae temporibus quia voluptatum fugit obcaecati facilis repudiandae ullam saepe laborum, autem exercitationem perferendis quidem atque adipisci fuga similique nihil, possimus a? Iusto culpa dolores modi, voluptate optio, ipsa voluptatem aperiam itaque a illo facilis alias, repellendus praesentium velit fuga provident ea aspernatur in cumque fugiat tenetur dignissimos deleniti. Totam ducimus asperiores dolorum et error! Animi quas libero quaerat eligendi praesentium sunt dolorem similique placeat vel, rerum velit corporis delectus. Consequuntur saepe illo deleniti, laudantium iusto harum earum atque tenetur at nisi dicta. Aut deleniti consectetur error eius? Labore vel tempora eveniet neque, dolor libero reiciendis non.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, adipisci blanditiis! Dolor in ullam rerum amet placeat provident quo unde?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, debitis accusamus nostrum tenetur eaque modi dolorem, earum iste, corporis quasi quisquam unde assumenda veritatis pariatur ipsa vitae ut neque labore corrupti quibusdam odit hic repellat magnam! Suscipit quia nostrum corrupti deserunt quaerat beatae sit, nulla iure aut magni velit praesentium culpa possimus non, aliquid officiis minima. Commodi magni officia tempora animi quibusdam veniam. Vel provident, ipsum saepe earum hic eos blanditiis sint est aliquid aliquam asperiores omnis dolore repudiandae recusandae odit consequuntur, sequi ad pariatur quibusdam aperiam vitae quisquam sunt magni. Ex maiores iste quo amet, commodi ad consequatur! Consectetur, eligendi excepturi porro explicabo fuga soluta possimus fugiat quos, numquam corporis mollitia libero unde eveniet voluptatibus voluptate ad deleniti esse hic iusto error necessitatibus a. Laudantium repudiandae enim commodi maiores blanditiis reiciendis ducimus ipsa beatae modi ullam ab cum illum aspernatur natus repellat nobis eaque unde voluptas, officia temporibus dolorem! Repellendus quos at sunt obcaecati veniam quibusdam, alias nesciunt sequi ea, quo vel. Iste dolorum esse tenetur corrupti nobis eum aliquid, sunt earum expedita dolores voluptatem sequi obcaecati reiciendis suscipit itaque ullam optio. Corrupti quas, quam illum architecto assumenda aliquid? At impedit porro dolor, quia ea laudantium expedita minima facere.
+        </p>
+      </div>
+    </section>
   )
 }
 
